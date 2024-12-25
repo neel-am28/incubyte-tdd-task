c6003f786a460eeef7b81f7084ca640315f205c3
Neelam Chaubey <neelamchaubey@Neelams-MacBook-Air.local>
2024-12-25 18:12:29 +0530

    add logic to handle cases if string contains only one digit

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,6 +2,11 @@ function add(numbers) {
   if (numbers.trim() == "") {
     return 0
   }
+
+  const numbersArray = numbers.split(",").map((num) => Number(num))
+  if (numbersArray.length == 1) {
+    return numbersArray[0]
+  }
 }
 
 module.exports = {add}
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -4,4 +4,8 @@ describe("string calculator", () => {
   it("should return 0 if empty string is passed", () => {
     expect(add("")).toBe(0)
   })
+
+  it("should return the first number if string length is one", () => {
+    expect(add("1")).toBe(1)
+  })
 })
